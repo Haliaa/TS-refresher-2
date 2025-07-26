4982a846ff2c71de8c8e88707bdb4d75b4f94ec8
Halia <91005548+Haliaa@users.noreply.github.com>
2025-07-26 16:03:10 +0300

    upd for success gh-pages deploy

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/TS-refresher-2/",
   plugins: [react()],
+  base: "/TS-refresher-2/",
 });
